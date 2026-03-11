44a27ec4964f3cdda0a479ab03dff283781e0662
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-03-11 20:58:41 +0530

    Updated Mobile Resume Section

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-alert("Welcome to KJK Resume Portfolio")
+// alert("Welcome to KJK Resume Portfolio")
 
 const links = document.querySelectorAll(".link");
 const loader = document.getElementById("loader");
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
+
+btn.onclick = function(){
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+
+const texts = [
+"Passionate Developer",
+"Problem Solver",
+"Lifelong Learner"
+];
+
+let i = 0;
+const el = document.getElementById("mobile-home-text");
+
+setInterval(() => {
+    i = (i + 1) % texts.length;
+    el.textContent = texts[i];
+}, 4000);
 
 loader.style.display = "none";
 
